721aa449aba8b0628ac866993c1227d7efea5006
luanfrinhani <49029493+luanfrinhani@users.noreply.github.com>
2020-11-21 19:35:42 -0300

    Update header.js

--- app/view/header.js
+++ app/view/header.js
@@ -2,7 +2,6 @@ function Header(){
     return (
         <nav className='title'>
             <h1>Unsplash 100 label classifier</h1>
-            <span className='ramaTwitter'><a href='https://twitter.com/ramgendeploy' target='_blank'>@ramgendeploy</a></span>
         </nav>
     )
 }
